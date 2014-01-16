25f57fb788d01d1849fbb126cd01fc21f6d7158c
Jason Sebring <mail@jasonsebring.com>
2014-01-16 15:47:33 -0800

    updates

---
+++ website/config/default.js
@@ -0,0 +1,13 @@
+var kitguiAccountKey = '[ FILL IN ]';
+module.exports = {
+	kitgui : {
+		host : 's3.amazonaws.com',
+		basePath : '/kitgui/clients/' + kitguiAccountKey,
+		accountKey : kitguiAccountKey
+	},
+	mailchimp : {
+		apikey : '[ FILL IN ]-us3',
+		url : 'https://us3.api.mailchimp.com/2.0/',
+		listID : '[ FILL IN ]'
+	}
+};
