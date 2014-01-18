785b144b5e5e2af77091410815092bf8059a1083
Jason Sebring <mail@jasonsebring.com>
2014-01-18 01:10:58 -0800

    fixed iOS scrolling bug

--- website/public/js/main.js
+++ website/public/js/main.js
@@ -1,3 +1,17 @@
+$(window).resize(function(){
+	$('#mobileMenu .nav').css({
+		height: 10000
+	});
+	if ($(window).data('iosorientationfix')) {
+		clearInterval($(window).data('iosorientationfix'));
+	}
+	$(window).data('iosorientationfix', setTimeout(function(){
+		var h = $(window).height();
+		$('#mobileMenu .nav').css({
+			height: 'auto'
+		});
+	},10));
+});
 $('#mobileNavToggle').click(function(ev){
 	ev.preventDefault();
 	var $mobileMenu = $('#mobileMenu'),
