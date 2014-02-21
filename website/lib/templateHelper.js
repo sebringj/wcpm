2e05534da91bfc57150e46096b1905fb4a16f6f2
Jason Sebring <mail@jasonsebring.com>
2014-02-20 18:22:51 -0800

    updates

--- website/lib/templateHelper.js
+++ website/lib/templateHelper.js
@@ -26,6 +26,50 @@ function template1(context, route) {
 			pageID : pageID,
 			items : [
 				{ id : pageID + 'Rotator', editorType : 'bootstrap-carousel-json' },
+				{ id : pageID + 'Title', editorType : 'inline' },
+				{ id : pageID + 'Image', editorType : 'bootstrap-carousel-json' },
+				{ id : pageID + 'Content', editorType : 'html' },
+				{ id : pageID + 'RightHeader', editorType : 'inline' },
+				{ id : pageID + 'RightContent', editorType : 'html' }
+			]
+		}, function(kg){
+			if (!routeOK && !kg.seo.title) {
+				return res.redirect('/404');
+			}
+			context.cache[pageID] = {
+				layout : context.cache.layout,
+				kitguiAccountKey : config.kitgui.accountKey,
+				pageID : pageID,
+				items : kg.items,
+				title : kg.seo.title,
+				description : kg.seo.description
+			};
+			render();
+		});
+	});
+}
+
+function template2(context, route) {
+	context.app.get(route, function(req, res){
+		var routeOK = false;
+		var pageID = cleanURL(req.path);
+		
+		function render() {
+			res.render('template2', context.cache[pageID]);
+		}
+		if (req.cookies.kitgui === '1') {
+			routeOK = true;
+			delete context.cache[pageID];
+		}
+		if (context.cache[pageID]) {
+			return render();
+		}
+		kitgui.getContents({
+			basePath : config.kitgui.basePath,
+			host : config.kitgui.host,
+			pageID : pageID,
+			items : [
+				{ id : pageID + 'Rotator', editorType : 'bootstrap-carousel-json' },
 				{ id : pageID + 'Title', editorType : 'inline' }
 			]
 		}, function(kg){
