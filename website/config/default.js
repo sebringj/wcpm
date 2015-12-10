b285445f00c22ec61031bd50c65f5ee521d72469
Jason Sebring <mail@jasonsebring.com>
2015-12-10 12:55:30 -0800

    config update

--- website/config/default.js
+++ website/config/default.js
@@ -1,15 +1,15 @@
 var kitguiAccountKey = 'a009f65654d241638cbb908f41f03f9b';
 module.exports = {
-	domain : 'www.westcoastprimemeats.com',
-	kitgui : {
-		host : 's3.amazonaws.com',
-		basePath : '/kitgui/clients/' + kitguiAccountKey,
-		accountKey : kitguiAccountKey
-	},
-	email : {
-	    user: 'noreply@emeraldcode.com',
-		to: 'webinfo@westcoastprimemeats.com',
-	    pass: 'Abc123!~!',
-		from: 'noreply@emeraldcode.com'
-	},
+  domain: 'www.westcoastprimemeats.com',
+  kitgui: {
+    host: 's3.amazonaws.com',
+    basePath: '/kitgui/clients/' + kitguiAccountKey,
+    accountKey: kitguiAccountKey
+  },
+  email: {
+    user: 'noreply@emeraldcode.com',
+    to: 'webinfo@westcoastprimemeats.com',
+    pass: process.env.EMAIL_PASSWORD,
+    from: 'noreply@emeraldcode.com'
+  },
 }; // 216.31.162.187
